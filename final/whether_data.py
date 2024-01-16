984403fe2963865e2177fbb593f62d38f3569597
matsumotohikari <s2322090@stu.musashino-u.ac.jp>
2024-01-16 14:51:56 +0900

    コードを修正

--- final/whether_data.py
+++ final/whether_data.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-import sqlite3
 
 url = 'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year=2023&month=12&day=&view=a2'
 
